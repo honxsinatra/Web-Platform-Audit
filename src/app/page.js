8df6bc9299e50352b9da72f736b919f97695e1f6
honx sinatra <josephhongoa2020@gmail.com>
2023-12-07 00:43:38 +0300

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,67 +1,77 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
+          Get started by visiting &nbsp;
+          <code className={styles.code}>Workwise</code>
         </p>
         <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+          <p>
+            By <h1>HONGOA</h1>
+          </p>
         </div>
       </div>
 
       <div className={styles.center}>
         <Image
           className={styles.logo}
-          src="/next.svg"
+          src="/workwisepicha.png"
           alt="Next.js Logo"
           width={180}
-          height={37}
+          height={57}
+          priority
+        />
+        <Image
+          className={styles.logo}
+          src="/internal.jpg"
+          alt="Next.js Logo"
+          width={250}
+          height={87}
+          priority
+        />
+        <Image
+          className={styles.logo}
+          src="/checklist.png"
+          alt="Next.js Logo"
+          width={250}
+          height={87}
           priority
         />
       </div>
 
       <div className={styles.grid}>
         <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="/HOMEPAGE.pdf"
           className={styles.card}
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2>
-            Docs <span>-&gt;</span>
+            Audit Checklist <span>-&gt;</span>
           </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
+          <p>
+            Explore the comprehensive checklist to review what has been audited
+            in-depth.
+          </p>
         </a>
 
         <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="https://workwise.africa/"
           className={styles.card}
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2>
-            Learn <span>-&gt;</span>
+            Visit Workwise <span>-&gt;</span>
           </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
+          <p>
+            Discover the audited platform and take advantage of a free trial to
+            explore its&nbsp;features!
+          </p>
         </a>
 
         <a
@@ -71,25 +81,24 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2>
-            Templates <span>-&gt;</span>
+            Audit Reports <span>-&gt;</span>
           </h2>
-          <p>Explore starter templates for Next.js.</p>
+          <p>Review the audit reports for the modules of the platform.</p>
         </a>
 
         <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="https://hongoa-portfolio.vercel.app/"
           className={styles.card}
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2>
-            Deploy <span>-&gt;</span>
+            Are you interested in having your web platform audited?{" "}
+            <span>-&gt;</span>
           </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
+          <p>Feel free to contact me through my portfolio.</p>
         </a>
       </div>
     </main>
-  )
+  );
 }
